1c0af350de564917a919c042d2884031396f91de
Claudio Villanueva <claudioff@gmail.com>
2026-03-27 11:11:30 -0300

    StackView added programmatically.

--- UIKitDemoProgrammatically/SceneDelegate.swift
+++ UIKitDemoProgrammatically/SceneDelegate.swift
@@ -16,7 +16,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        // Customized setup to run App without Storyboards
+        if let windowScene = scene as? UIWindowScene {
+
+            let window = UIWindow(windowScene: windowScene)
+            let viewController = ViewController()
+
+            let navigation = UINavigationController(
+                rootViewController: viewController
+            )
+            window.rootViewController = navigation
+
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
